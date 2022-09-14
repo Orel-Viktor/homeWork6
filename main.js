736efb9a0c06e42a86049c31bac5e727ac55e6c2
Orel-Viktor <OrelViktor93@gmail.com>
2022-09-14 15:25:56 +0300

    homework6-redy

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
-import './style.css'
+// 1. Используя цикл, умножьте все числовые свойства объекта menu на 2. 
+// После умножения, удалите свойства width и height. 
+// А title замените на строку "Clear"
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+  
+}
+ for (let prop in menu){
+  if (typeof menu[prop] == 'number'){
+    menu[prop] *= 2
+  }
+ }
+ console.log (menu)
+ 
+ delete menu.width
+ delete menu.height
+ menu.title = 'Clear'
+
+ console.log (menu)
+
+
+
+// ЭТО Я СОЗДАЛ КЛОН , так что бы если я менял основной обьект , клон не менялся , гляньте правильно ли.
+
+// let menuClone = {...menu}
+
+// for (let prop in menu){
+// if (typeof menu[prop] == 'number'){
+//   menuClone[prop] = menu[prop] *2
+// }
+// } 
+
+// console.log( 'Клон', menuClone ) 
+// console.log(menu)
+
+// delete menu.width 
+// delete menu.height 
+//  menu.title  = 'Clear' 
+
+ 
+// console.log( 'Клон','Убрал свойства у оригинала', menuClone) 
+// console.log(menu)
+
+
+
+
+
